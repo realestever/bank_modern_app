93e856d7362dd2b5200cf950f4d9d7575de511c5
realestever <119549369+realestever@users.noreply.github.com>
2025-08-28 11:06:15 -0700

    created button class

--- bank-modern-app/src/components/Button.jsx
+++ bank-modern-app/src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({styles}) => {
   return (
-    <div>
-      
-    </div>
+    <button type='button' className={`bg-blue-gradient rounded-xl py-4 px-6 cursor-pointer outline-none ${styles}`}>
+      <p className='font-poppins font-medium text-[18px] text-primary ' >Get Started</p>
+    </button>
   )
 }
 
