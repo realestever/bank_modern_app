f93b9aee09b4b972a6e898a4b501c42ccb628631
realestever <119549369+realestever@users.noreply.github.com>
2025-08-29 13:01:59 -0700

    card section done

--- bank-modern-app/src/components/Button.jsx
+++ bank-modern-app/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({styles}) => {
   return (
-    <button type='button' className={`bg-blue-gradient rounded-xl py-4 px-6 cursor-pointer outline-none ${styles}`}>
+    <button type='button' className={`bg-blue-gradient py-4 px-6 cursor-pointer outline-none ${styles} rounded-[10px]`}>
       <p className='font-poppins font-medium text-[18px] text-primary ' >Get Started</p>
     </button>
   )
